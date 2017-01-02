1caf5b1d7962a0ce875a6c96102d372246a18c28
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-01-02 16:30:14 +0100

    TeamBuiltingStatistic: Starting the functionality

--- src/main/java/org/manuel/teambuilting/experience/config/RabbitMQConfig.java
+++ src/main/java/org/manuel/teambuilting/experience/config/RabbitMQConfig.java
@@ -2,22 +2,27 @@ package org.manuel.teambuilting.experience.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.text.SimpleDateFormat;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.text.SimpleDateFormat;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 /**
  * @author Manuel Doncel Martos
  * @since 07/12/2016.
  */
 @Configuration
-public class RabbitMQConfig  {
+public class RabbitMQConfig implements RabbitListenerConfigurer {
 
     private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd\'T\'HH:mm:ss.SSSZ";
 
@@ -34,6 +39,24 @@ public class RabbitMQConfig  {
 
     public Queue queue() {
         return new Queue(teamQueueName, true);
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter jackson2Converter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        return converter;
+    }
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory myHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(jackson2Converter());
+        return factory;
+    }
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(myHandlerMethodFactory());
     }
 
     @Bean(name = "eventMessageConverter")
