096e366e2dd6f087ba0edfa903576f95100e9e63
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-01-08 20:05:56 +0100

    TeamBuiltingExperience: Skeleton of the player rewards

---
+++ src/main/java/org/manuel/teambuilting/experience/rewards/Reward.java
@@ -0,0 +1,10 @@
+package org.manuel.teambuilting.experience.rewards;
+
+/**
+ * @author Manuel Doncel Martos
+ * @since 04/01/2016.
+ */
+public enum Reward {
+
+    BEST_PLAYER, TOP_SCORER, WORST_ONE, BEST_GOAL, WHO_IS_HE, BEST_COACH, THE_INJURY;
+}
