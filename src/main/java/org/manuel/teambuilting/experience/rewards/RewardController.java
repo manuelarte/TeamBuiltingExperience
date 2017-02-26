9207990973f3e18b84774efbb552b7afdf70769e
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-02-26 21:44:43 +0100

    TeamBuiltingExperience: changing the rest endpoint of the rewards

--- src/main/java/org/manuel/teambuilting/experience/rewards/RewardController.java
+++ src/main/java/org/manuel/teambuilting/experience/rewards/RewardController.java
@@ -1,24 +1,24 @@
 package org.manuel.teambuilting.experience.rewards;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Manuel Doncel Martos
  * @since 04/01/2016.
  */
 @RestController
-@RequestMapping("/rewards")
+@RequestMapping("experience/rewards")
 public class RewardController {
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public Set<Reward> getCommentsFor() {
+    public Set<Reward> getAllRewards() {
         return Arrays.stream(Reward.values()).collect(Collectors.toSet());
     }
 
